4b54a8b84e85709e066bd6c689ef676b456f2e68
Pablo Barahona <pbarahona07@gmail.com>
2023-02-28 18:34:59 -0600

    librerias cargadas

--- BinaryTrees/src/Errores/Excepcion.java
+++ BinaryTrees/src/Errores/Excepcion.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Errores;
 
-/**
- *
- * @author Pilo Tuy
- */
+
 public class Excepcion {
     private String tipo;
     private String descripcion;
